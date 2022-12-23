afad6fef840d6e0fdb43e214aa0a2850ff70ddae
LinTingr <a0925391958@gmail.com>
2022-12-23 13:51:56 +0800

    add mvc

---
+++ model/order.py
@@ -0,0 +1,43 @@
+from model.taipeidb import cnxpool
+from flask import *
+
+class Order:
+    def payorder(orderdata):
+        try :
+            cnx = cnxpool.get_connection()
+            cursor = cnx.cursor()
+            query = "INSERT INTO orders(userid, number, attractionId, date, time, price, name, email, phone) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            cursor.execute(query, orderdata)
+            cnx.commit()
+        finally:
+            cursor.close()
+            cnx.close()
+
+    def payorder_alter(number):
+        try :
+            cnx = cnxpool.get_connection()
+            cursor = cnx.cursor()
+            query = "UPDATE orders SET status = 0 where number = %s;"
+            cursor.execute(query, (number,))
+            cnx.commit()
+        finally:
+            cursor.close()
+            cnx.close()
+
+    def get_order(number):
+        try :
+            cnx = cnxpool.get_connection()
+            cursor = cnx.cursor(dictionary=True)
+            query = """select orders.price, orders.date, orders.time, orders.name, orders.email, orders.phone, orders.status,
+            attraction.id, attraction.name as attractionName, attraction.address, attraction.images
+            from orders 
+            INNER JOIN attraction
+            ON  orders.attractionId = attraction.id
+            where orders.number = %s """
+            print(number)
+            cursor.execute(query, (number,))
+            data = cursor.fetchone()
+            return data
+        finally:
+            cursor.close()
+            cnx.close()
