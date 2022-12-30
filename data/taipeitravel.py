d5b7821395279324a32179f6459370058845cfff
LinTingr <a0925391958@gmail.com>
2022-12-30 08:13:51 +0800

    modify

--- data/taipeitravel.py
+++ data/taipeitravel.py
@@ -19,27 +19,38 @@ with open(path, encoding='UTF-8') as response:
 # print(data)
 taipeilist = data["result"]["results"]
 # print(taipeilist)
-for i in taipeilist:
-    query = "INSERT into attraction(id, name, category, description, address, transport, mrt, lat, lng, images) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    id = i["_id"]
-    name = i["name"]
-    category = i["CAT"]
-    description = i["description"]
-    address = i["address"]
-    transport = i["direction"]
-    mrt = i["MRT"]
-    lat = i["latitude"]
-    lng = i["longitude"]
-    files = i["file"].split("https")
-    images = []
-    for j in files:
-        if j.endswith("jpg")  or j[-3:] == "JPG":
-            images.append("https" + j )
-    images = ", ".join(images)
-    cursor.execute(query, (id, name, category, description, address, transport, mrt, lat, lng, images))
-    cnx.commit()
+# for i in taipeilist:
+#     query = "INSERT into attraction(id, name, category, description, address, transport, mrt, lat, lng, images) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+#     id = i["_id"]
+#     name = i["name"]
+#     category = i["CAT"]
+#     description = i["description"]
+#     address = i["address"]
+#     transport = i["direction"]
+#     mrt = i["MRT"]
+#     lat = i["latitude"]
+#     lng = i["longitude"]
+#     files = i["file"].split("https")
+#     images = []
+#     for j in files:
+#         print(j)
+#         if j.endswith("jpg")  or j[-3:] == "JPG":
+#             images.append("https" + j )
+#     images = ", ".join(images)
+#     cursor.execute(query, (id, name, category, description, address, transport, mrt, lat, lng, images))
+#     cnx.commit()
     # print(images)
-
+for i in taipeilist:
+    query = "INSERT into attraction_image(attractionId, image) VALUES(%s, %s);"
+    attractionId = i["_id"]
+    files = i["file"].split("https")
+    for j in files:
+        # print(j)
+        if j.endswith("jpg")  or j[-3:] == "JPG":
+            image = "https" + j
+            cursor.execute(query, (attractionId, image))
+            cnx.commit()
+            # print(image)
 
 
 cursor.close()  
